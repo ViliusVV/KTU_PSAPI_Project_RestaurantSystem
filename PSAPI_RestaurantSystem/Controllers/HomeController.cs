dc389d77cbcd7d9512c28cbaf15d0b8732a9b4f3
ViliusVV <33135408+ViliusVV@users.noreply.github.com>
2020-04-13 17:25:10 +0300

    Add project files.

---
+++ PSAPI_RestaurantSystem/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PSAPI_RestaurantSystem.Models;
+
+namespace PSAPI_RestaurantSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogWarning("OMG. Someone is reading privacy TOS!!!11!1!");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
