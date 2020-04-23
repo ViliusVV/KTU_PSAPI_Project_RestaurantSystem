64f91fbfbec4c17e4c1c93334f42854e2f97a463
ViliusVV <33135408+ViliusVV@users.noreply.github.com>
2020-04-23 18:04:42 +0300

    Database context. Migrations

--- PSAPI_RestaurantSystem/RestaurantContext.cs
+++ PSAPI_RestaurantSystem/RestaurantContext.cs
@@ -11,6 +11,17 @@ namespace PSAPIRestaurantSystem
     {
         public DbSet<Person> People { get; set; }
         public DbSet<Table> Tables { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuEntry> MenuEntries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderedMeal> OrderedMeals { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<TakeoutOrder> TakeoutOrders { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Waiter> Waiters { get; set; }
 
         public RestaurantContext(DbContextOptions<RestaurantContext> options): base(options)
         {
