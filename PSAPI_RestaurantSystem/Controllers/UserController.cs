93a5a55d22b8324a04272357e11532b67efaba70
Rocco <rokas.kran@gmail.com>
2020-05-16 16:15:02 +0300

    login in progress

--- PSAPI_RestaurantSystem/Controllers/UserController.cs
+++ PSAPI_RestaurantSystem/Controllers/UserController.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PSAPIRestaurantSystem.Models.ViewModels;
 
 namespace PSAPIRestaurantSystem.Controllers
 {
     public class UserController : Controller
     {
         private readonly RestaurantContext _context;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(RestaurantContext context)
+        public UserController(RestaurantContext context, ILogger<UserController> logger)
         {
+            _logger = logger;
             _context = context;
         }
 
@@ -21,6 +25,18 @@ namespace PSAPIRestaurantSystem.Controllers
         {
             var restaurantContext = _context.Menus.Include(m => m.MenuEntries);
             return View(restaurantContext.ToList());
+        }
+        public IActionResult LoginForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginForm(LoginViewModel model)
+        {
+            _logger.LogDebug(model.LoginName);
+            var usr = _context.Users.Select(u => u.Email == model.LoginName).FirstOrDefault();
+
+            return View();
         }
     }
 }
---
+++ PSAPI_RestaurantSystem/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSAPIRestaurantSystem.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name="Prisijungimo vardas")]
+        public string LoginName { get; set; }
+        [Display(Name = "Slaptažodis")]
+        public string Password { get; set; }
+    }
+}
